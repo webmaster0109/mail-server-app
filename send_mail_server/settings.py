7d5e8ae5724f321b12815b5bb9962c6cd7fcf469
Shrichand Thapa <154520287+webmaster0109@users.noreply.github.com>
2025-06-05 18:49:04 +0530

    Update settings.py

--- send_mail_server/settings.py
+++ send_mail_server/settings.py
@@ -67,13 +67,23 @@ WSGI_APPLICATION = 'send_mail_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+      'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'amaramailserver',
+            'USER': 'amara_mail',
+            'PASSWORD': 'amara_mail',
+            'HOST': 'amara_mysql',
+            'PORT': '3306',
+      }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
